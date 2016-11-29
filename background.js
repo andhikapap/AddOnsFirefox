68427fd098dfd9f1f374b6665c8951e4d473c5a5
andhikapap <andhika.pap@gmail.com>
2016-11-29 23:08:07 +0700

    versi awal

---
+++ background.js
@@ -0,0 +1,7 @@
+function openMyPage(){
+	console.log("injecting");
+	chrome.tabs.create({
+		"url": chrome.extension.getURL("https://web.whatsapp.com")
+	});
+}
+chrome.browserAction.onClicked.addListener(openMyPage);
